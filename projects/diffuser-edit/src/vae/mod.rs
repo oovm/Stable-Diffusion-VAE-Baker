f1c03f00910ab9d33c3baf6b5358d38482f63e51
Aster <aster@vers.site>
2023-05-30 21:50:40 +0000

    🧪 Support bake stable diffusion v1.5

--- projects/diffuser-edit/src/vae/mod.rs
+++ projects/diffuser-edit/src/vae/mod.rs
@@ -1,20 +1,82 @@
-use candle_core::{Device, DType, Tensor, Var};
+use candle_core::{Tensor};
 use std::collections::HashMap;
+use std::path::Path;
+use candle_core::safetensors::save;
+use crate::helpers::load_model;
 
 
-pub fn bake_vae(mut checkpoint: HashMap<String, Tensor>, vae: &HashMap<String, Tensor>) -> candle_core::Result<HashMap<String, Tensor>> {
-    // Filter VAE dictionary to exclude keys starting with "loss" or "mode"
-    let vae_dict: HashMap<String, Tensor> = vae.iter()
-        .filter(|(k, _)| !k.starts_with("loss") && !k.starts_with("mode"))
-        .map(|(k, v)| (k.clone(), v.clone()))
-        .collect();
-    for (k, v) in vae_dict.iter() {
-        println!("{}", k)
+pub fn bake_vae_by_path(checkpoint: &Path, vae: &Path) -> candle_core::Result<()> {
+    tracing::info!("Loading VAE: {}", vae.display());
+    let vae_weight = load_model(vae)?;
+    if vae_weight.is_empty() {
+        tracing::error!("No VAE weights found in {}", vae.display());
+        return Ok(());
     }
-    for (k, v) in vae_dict.iter() {
-        let key_name = format!("first_stage_model.{}", k);
-        checkpoint.insert(key_name, v.clone());
+    tracing::info!("Loading Stable Diffusion: {}", checkpoint.display());
+    let checkpoint_weight = load_model(checkpoint)?;
+    let output = bake_vae_sd15(checkpoint_weight, &vae_weight)?;
+    let name = format!("{}-{}.safetensors", checkpoint.file_stem().unwrap().to_str().unwrap(), vae.file_stem().unwrap().to_str().unwrap());
+    tracing::info!("Saving Baked Stable Diffusion: {}", name);
+    save(&output, name)
+}
+
+pub fn bake_vae(checkpoint: HashMap<String, Tensor>, vae: &HashMap<String, Tensor>) -> candle_core::Result<HashMap<String, Tensor>> {
+    bake_vae_sd15(checkpoint, vae)
+}
+
+
+fn bake_vae_sd15(mut checkpoint: HashMap<String, Tensor>, vae: &HashMap<String, Tensor>) -> candle_core::Result<HashMap<String, Tensor>> {
+    tracing::info!("Baking Stable Diffusion v1.5 VAE...");
+    // Filter VAE dictionary to exclude keys starting with "loss" or "mode"
+    for (k, v) in vae.iter() {
+        let weight = match k.as_str() {
+            w if w.starts_with("first_stage_model.") => {
+                w.to_string()
+            }
+            w if w.starts_with("encoder") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("decoder") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("quant_conv") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("post_quant_conv") => {
+                format!("first_stage_model.{w}")
+            }
+            _ => continue,
+        };
+        tracing::info!("    Bake: {}", weight);
+        checkpoint.insert(weight, v.clone());
     }
     Ok(checkpoint)
 }
 
+fn bake_vae_sdxl(mut checkpoint: HashMap<String, Tensor>, vae: &HashMap<String, Tensor>) -> candle_core::Result<HashMap<String, Tensor>> {
+    tracing::info!("Baking Stable Diffusion v1.5 VAE...");
+    // Filter VAE dictionary to exclude keys starting with "loss" or "mode"
+    for (k, v) in vae.iter() {
+        let weight = match k.as_str() {
+            w if w.starts_with("first_stage_model.") => {
+                w.to_string()
+            }
+            w if w.starts_with("encoder") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("decoder") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("quant_conv") => {
+                format!("first_stage_model.{w}")
+            }
+            w if w.starts_with("post_quant_conv") => {
+                format!("first_stage_model.{w}")
+            }
+            _ => continue,
+        };
+        tracing::info!("    Bake: {}", weight);
+        checkpoint.insert(weight, v.clone());
+    }
+    Ok(checkpoint)
+}
