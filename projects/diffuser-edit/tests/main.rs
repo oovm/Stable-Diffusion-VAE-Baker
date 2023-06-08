bf3cb8ade431d0d1cc310b175ef8ea1087890768
Aster <galaster@foxmail.com>
2023-06-08 20:19:33 +0000

    🚀 Skip jpg decoding

--- projects/diffuser-edit/tests/main.rs
+++ projects/diffuser-edit/tests/main.rs
@@ -1,32 +1,10 @@
-use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
-use std::fs::File;
-use diffuser_edit::{bake_vae_by_path, ImageProcessing};
-use std::path::{Path, PathBuf};
-use candle_core::{DType, Device, Tensor};
-use candle_core::safetensors::Load;
+use std::path::{Path};
 use safetensors::SafeTensors;
-use safetensors::tensor::TensorView;
 
 #[test]
 fn ready() {
     println!("it works!")
-}
-
-#[test]
-#[ignore]
-fn run() -> candle_core::Result<()> {
-    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
-    bake_deep_ocean()?;
-    // bake_shiny_sky()
-    Ok(())
-}
-
-fn bake_deep_ocean() -> candle_core::Result<()> {
-    let here = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let model = here.join("tests/hassakuXLHentai_v13BetterEyesVersion.safetensors");
-    let vae = here.join("tests/xlVAEC_f1.safetensors");
-    bake_vae_by_path(&model, &vae)
 }
 
 // fn bake_shiny_sky() -> candle_core::Result<()> {
@@ -35,16 +13,6 @@ fn bake_deep_ocean() -> candle_core::Result<()> {
 //     let vae = here.join("tests/kl-f8-anime2.vae.safetensors");
 //     bake_vae_by_path(&model, &vae)
 // }
-
-#[test]
-fn text_em() {
-    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/badhandv4.pt");
-    let array: HashMap<String, Tensor> = candle_core::pickle::read_all_with_key(path, None).unwrap().into_iter().collect();
-    for (k, v) in array.iter() {
-        println!("{}: {:?}", k, v.dtype());
-    }
-}
-
 
 pub fn sign_model(path: &str, path2: &Path) -> Result<(), Box<dyn Error>> {
     let safetensors_file = std::fs::read(path)?;
@@ -65,22 +33,11 @@ pub fn sign_model(path: &str, path2: &Path) -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
+#[ignore]
 fn main3() {
     let dir = sign_model(
         r#"C:\Users\Aster\Downloads\realisticFreedom3_auroraV09.safetensors"#,
         &Path::new(r#"C:\Users\Aster\Downloads\XE_UNREAL_SD3.safetensors"#),
     );
     // step.convert_directory(&dir)
-}
-
-
-#[test]
-fn main2() {
-    let dir = Path::new(r#"C:\Users\Aster\Downloads"#);
-    let step = ImageProcessing {
-        crop_alpha: true,
-        erase_alpha: true,
-        delete_source: true,
-    };
-    step.convert_directory(&dir)
 }
