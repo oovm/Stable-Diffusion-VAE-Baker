6cc88fc177a2cf299fe188958a7446ab7692faab
Aster <aster@vers.site>
2024-06-14 21:25:12 +0800

    🧪 Support quantize as float16

--- projects/diffuser-edit/src/helpers/mod.rs
+++ projects/diffuser-edit/src/helpers/mod.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::path::Path;
-use candle_core::{Device, Tensor};
+use candle_core::{Device, DType, Tensor};
 
 pub fn load_model(path: &Path) -> candle_core::Result<HashMap<String, Tensor>> {
     let tensors = match path.extension() {
@@ -15,4 +15,26 @@ pub fn load_model(path: &Path) -> candle_core::Result<HashMap<String, Tensor>> {
         }
     };
     Ok(tensors)
+}
+
+/// Quantize the float tensors to f16.
+pub fn quantize_f16(checkpoint: &mut HashMap<String, Tensor>) -> candle_core::Result<()> {
+    for (k, v) in checkpoint.iter_mut() {
+        match v.dtype() {
+            DType::U8 => {}
+            DType::U32 => {}
+            DType::I64 => {}
+            DType::BF16 => {}
+            DType::F16 => {}
+            DType::F32 => {
+                tracing::info!("    Quantize: f32 `{}` to f16", k);
+                *v = v.to_dtype(DType::F16)?
+            }
+            DType::F64 => {
+                tracing::info!("    Quantize: f64 `{}` to f16", k);
+                *v = v.to_dtype(DType::F16)?
+            }
+        }
+    }
+    Ok(())
 }
